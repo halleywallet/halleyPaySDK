8511786177655f0374bc10394de9a1796423109f
Mohammad Takbiri <mohammad_online2013@yahoo.com>
2023-10-09 09:44:08 +0330

    َUpdate the SDK

--- Sources/HalleyPayFramework.xcframework/ios-arm64_x86_64-maccatalyst/HalleyPayFramework.framework/Versions/A/Headers/HalleyPayFramework-Swift.h
+++ Sources/HalleyPayFramework.xcframework/ios-arm64_x86_64-maccatalyst/HalleyPayFramework.framework/Versions/A/Headers/HalleyPayFramework-Swift.h
@@ -301,6 +301,9 @@ typedef unsigned int swift_uint4  __attribute__((__ext_vector_type__(4)));
 
 
 
+
+
+
 #endif
 #if __has_attribute(external_source_symbol)
 # pragma clang attribute pop
@@ -608,6 +611,9 @@ typedef unsigned int swift_uint4  __attribute__((__ext_vector_type__(4)));
 #endif
 
 #if defined(__OBJC__)
+
+
+
 
 
 
